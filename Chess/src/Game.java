98d9adf29026d5f3ce96faa18b15687e1d78e438
JaledMC <j.moustafacalvo@gmail.com>
2018-12-24 17:03:49 +0100

    Update Game.java

--- Chess/src/Game.java
+++ Chess/src/Game.java
@@ -1,11 +1,3 @@
-/*
-POr que el tablero es tipo piece
-por que no se inicializa de 8x8 al principio
-porque hay una clase para movimiento
-porque no
-para la posicion mejor una tupla
-* */
-
 public class Game {
     Piece [][] board;
 
